afd17d50f017c1b77317edd8f86301f5861ddd0a
Aldi Kurniawan <aldikurw@gmail.com>
2021-07-18 19:43:09 +0700

    db

--- certainty_factor.sql
+++ certainty_factor.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 16 Jul 2021 pada 23.21
+-- Waktu pembuatan: 18 Jul 2021 pada 14.42
 -- Versi server: 10.4.19-MariaDB
 -- Versi PHP: 7.4.20
 
@@ -93,6 +93,81 @@ INSERT INTO `interests` (`id`, `name`) VALUES
 ('I0054', 'Saya suka melakukan aktivitas di alam terbuka atau diluar ruangan'),
 ('I0055', 'Saya lebih suka praktek langsung ketika mempelajari sesuatu'),
 ('I0056', 'Saya suka bergerak dan cepat bosan ketika disuruh duduk dalam waktu yang lama');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `interests_v2`
+--
+
+CREATE TABLE `interests_v2` (
+  `id` varchar(5) NOT NULL,
+  `name` varchar(200) NOT NULL,
+  `type_id` varchar(5) NOT NULL,
+  `mb` float DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `interests_v2`
+--
+
+INSERT INTO `interests_v2` (`id`, `name`, `type_id`, `mb`) VALUES
+('I0001', 'Saya suka membaca buku', 'T0002', 0.2),
+('I0002', 'Suka menulis', 'T0002', 0.2),
+('I0003', 'Suka bercerita / berbicara', 'T0002', 0.2),
+('I0004', 'Suka membuat atau mengarang cerita', 'T0002', 0.2),
+('I0005', 'Saya suka membahas ide-ide yang saya punya dengan teman', 'T0002', 0.2),
+('I0006', 'Saya suka teka teki huruf', 'T0002', 0.2),
+('I0007', 'Saya suka belajar Bahasa Indonesia atau Bahasa Asing', 'T0002', 0.2),
+('I0008', 'Saya menyukai flora dan fauna serta alam semesta', 'T0008', 0.2),
+('I0009', 'Saya suka tentang alam', 'T0008', 0.2),
+('I0010', 'Saya peduli dengan alam dan lingkungan sekitar', 'T0008', 0.2),
+('I0011', 'Saya suka mengunjungi taman atau berjalan jalan di alam bebas', 'T0008', 0.2),
+('I0012', 'Suka camping atau menjelajahi alam', 'T0008', 0.2),
+('I0013', 'Suka mengumpulkan serangga, batu, daun atau koleksi alam lainnya di dalam botol / suatu wadah', 'T0008', 0.2),
+('I0014', 'Saya menikmati ketika kegiatan memancing ikan', 'T0008', 0.2),
+('I0015', 'Saya suka memikirkan masa depan dan tujuan-tujuan yang ingin dicapai', 'T0003', 0.2),
+('I0016', 'Saya lebih senang di rumah dan menghabiskan waktu sendiri', 'T0003', 0.2),
+('I0017', 'Saya lebih suka bekerja atau belajar sendiri daripada bersama teman', 'T0003', 0.2),
+('I0018', 'Saya suka melamun atau berpikir tentang kehidupan dan diri saya', 'T0003', 0.2),
+('I0019', 'Saya mengetahui kelebihan dan kekurangan diri saya', 'T0003', 0.2),
+('I0020', 'Saya suka menulis catatan harian untuk menuangkan ide-ide, kenangan, perasaan, peristiwa yang terjadi, dll\r\n', 'T0003', 0.2),
+('I0021', 'Saya suka berpikir atau mempertimbangkan sisi positif atau negatif ketika menemui masalah atau mengambil keputusan', 'T0003', 0.2),
+('I0022', 'Saya suka pelajaran matematika', 'T0007', 0.2),
+('I0023', 'Saya suka permainan yang menggunakan strategi atau mengasah otak, seperti catur, permainan misteri, teka-teki logika', 'T0007', 0.2),
+('I0024', 'Saya suka menanyakan tentang bagaimana suatu benda bekerja', 'T0007', 0.2),
+('I0025', 'Saya suka mengerjakan atau menyelesaikan perhitungan matematika dalam pikiran saya', 'T0007', 0.2),
+('I0026', 'Saya suka mengatur berbagai hal dengan teratur, kategoris dan hieraekis (berurutan)', 'T0007', 0.2),
+('I0027', 'Saya mudah mengingat berkaitan dengan angka atau statistik, seperti skor sepak bola, tinggi gedung tertinggi di dunia, dll', 'T0007', 0.2),
+('I0028', 'Saya dapat berhitung dengan cepat', 'T0007', 0.2),
+('I0029', 'Saya suka bergaul dan berkumpul dengan orang lain', 'T0004', 0.2),
+('I0030', 'Saya mudah berteman dan berbicara dengan orang yang baru dikenal', 'T0004', 0.2),
+('I0031', 'Saya lebih suka belajar bersama dengan teman dibanding belajar sendiri', 'T0004', 0.2),
+('I0032', 'Saya suka menawarkan bantuan ketika orang lain membutuhkannya', 'T0004', 0.2),
+('I0033', 'Saya mudah menebak perasaan teman hanya dengan melihat ekspresinya', 'T0004', 0.2),
+('I0034', 'Saya tahu bagaimana cara membuat teman bersemangat', 'T0004', 0.2),
+('I0035', 'Teman saya sering datang  untuk curhat, mencari dukungan emosi atau meminta saran', 'T0004', 0.2),
+('I0036', 'Saya suka memainkan alat musik', 'T0006', 0.2),
+('I0037', 'Saya senang bernyanyi', 'T0006', 0.2),
+('I0038', 'Saya dapat mengingat melodi atau nada dari lagu', 'T0006', 0.2),
+('I0039', 'Saya mudah mengenali banyak lagu yang berbeda beda', 'T0006', 0.2),
+('I0040', 'Saya suka seperti bertepuk tangan, menjentikkan jari, menghentakkan kaki, memukul meja seperti drum, dll', 'T0006', 0.2),
+('I0041', 'Saya sering bernyanyi ketika sedang melakukan aktifitas', 'T0006', 0.2),
+('I0042', 'Saya suka mengarang atau menulis lagu', 'T0006', 0.2),
+('I0043', 'Saya suka merangkai puzzle atau lego', 'T0005', 0.2),
+('I0044', 'Saya suka aktifitas fotografi', 'T0005', 0.2),
+('I0045', 'Saya suka menggambar atau melukis', 'T0005', 0.2),
+('I0046', 'Saya suka belajar dengan mengamati orang sekitar mengerjakan sesuatu', 'T0005', 0.2),
+('I0047', 'Saya lebih cepat memahami sesuatu dalam bentuk gambar, grafik atau ilustrasi', 'T0005', 0.2),
+('I0048', 'Saya mudah mengenali atau mengingat tempat atau jalan, walaupun baru pertama kali mengunjunginya', 'T0005', 0.2),
+('I0049', 'Saya lebih mudah mengingat wajahnya dibandingkan mengingat namanya', 'T0005', 0.2),
+('I0050', 'Saya suka mata pelajaran olahraga', 'T0001', 0.2),
+('I0051', 'Saya suka mengetukkan jari, memainkan alat tulis atau menggoyangkan kaki saat belajar atau berpikir', 'T0001', 0.2),
+('I0052', 'Saya lebih suka bergerak ketika mempelajari sesuatu untuk lebih membantu saya mengingat', 'T0001', 0.2),
+('I0053', 'Saya suka bermain sandiwara (acting) atau menari', 'T0001', 0.2),
+('I0054', 'Saya suka melakukan aktivitas di alam terbuka atau diluar ruangan', 'T0001', 0.2),
+('I0055', 'Saya lebih suka praktek langsung ketika mempelajari sesuatu', 'T0001', 0.2),
+('I0056', 'Saya suka bergerak dan cepat bosan ketika disuruh duduk dalam waktu yang lama', 'T0001', 0.2);
 
 -- --------------------------------------------------------
 
@@ -287,18 +362,17 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `username`, `password`, `full_name`, `privilege`, `avatar_id`, `last_login`) VALUES
-('U0001', 'yusuf', '$2y$10$yS28JUyLyPQqBHPWdLFmMOdeS2pc1g0mj.ttjYAJv.p04se.tGt5q', 'Yusuf Effendi', 'Admin', 2, '2021-07-14 01:36:29'),
-('U0002', 'aldikurw', '$2y$10$poD7TkNcO1WcDJTKmk.mbOz05BQeM.98An3yHJKE3k5aLg1kbbMne', 'Aldi Kurniawan', 'User', 1, '2021-07-11 03:00:27'),
-('U0003', 'edit', '$2y$10$T7tFgUt18x1/GDyh7dchxuOGA.BBzfAKIdznDqzefXBQe6HBMGesC', 'hhhbk', 'User', 3, NULL);
+('U0001', 'yusuf', '$2y$10$DYwZUHvSBPjCfQD.st7DhuzEMMd6/Andqb8BZ1r0TQ/uWobiYATuK', 'Yusuf Effendi', 'Admin', 2, '2021-07-14 01:36:29'),
+('U0002', 'aldikurw', '$2y$10$poD7TkNcO1WcDJTKmk.mbOz05BQeM.98An3yHJKE3k5aLg1kbbMne', 'Aldi Kurniawan', 'User', 1, '2021-07-11 03:00:27');
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indeks untuk tabel `interests`
+-- Indeks untuk tabel `interests_v2`
 --
-ALTER TABLE `interests`
+ALTER TABLE `interests_v2`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -369,14 +443,14 @@ ALTER TABLE `results`
 -- Ketidakleluasaan untuk tabel `results_details`
 --
 ALTER TABLE `results_details`
-  ADD CONSTRAINT `results_details_ibfk_2` FOREIGN KEY (`interest_id`) REFERENCES `interests` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `results_details_ibfk_2` FOREIGN KEY (`interest_id`) REFERENCES `interests_v2` (`id`) ON DELETE CASCADE,
   ADD CONSTRAINT `results_details_ibfk_3` FOREIGN KEY (`result_id`) REFERENCES `results` (`id`) ON DELETE CASCADE;
 
 --
 -- Ketidakleluasaan untuk tabel `rules`
 --
 ALTER TABLE `rules`
-  ADD CONSTRAINT `rules_ibfk_1` FOREIGN KEY (`interest_id`) REFERENCES `interests` (`id`),
+  ADD CONSTRAINT `rules_ibfk_1` FOREIGN KEY (`interest_id`) REFERENCES `interests_v2` (`id`),
   ADD CONSTRAINT `rules_ibfk_2` FOREIGN KEY (`type_id`) REFERENCES `types` (`id`);
 
 --
